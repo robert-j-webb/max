1e5d98f3a3bfe33bab472c2021bcd3e916ddc694
Robert Webb <robertwebb@modular.com>
2024-08-09 11:19:32 -0700

    Add usage stats to llama serve, and to rag

--- examples/gui/pages/rag.py
+++ examples/gui/pages/rag.py
@@ -13,7 +13,7 @@
 from pathlib import Path
 import os
 import subprocess
-
+import datetime
 import openai
 import streamlit as st
 import chromadb
@@ -25,6 +25,7 @@ from shared import download_file, kill_process, menu, modular_cache_dir
 st.set_page_config("RAG with Llama3", page_icon="🗣️ 📄")
 
 menu()
+_TEXT_CURSOR = "▕🔥"
 
 os.environ["TOKENIZERS_PARALLELISM"] = "0"
 
@@ -37,26 +38,23 @@ Supported formats: `.txt, .pdf, .csv, .docx, .epub, .ipynb, .md, .html`.
 For LLM, select a quantization encoding to download model from a predefined `Model URL`.
 If the model exists at `Model Path` it won't be downloaded again.
 You can set a custom `Model URL` or `Model Path` that matches the quantization encoding.
+
+Keep the output to one sentence of 10 words.
 """
 
 SYSTEM_PROMPT = st.sidebar.text_area(
     "System Prompt",
     value="""You are a helpful document search assistant.
     Your task is to find an answer to user's QUERY about their given documentations.
-    Be helpful and concise.
-    Think step by step.
-    DO NOT HALLUCINATE.""",
+    Be helpful.
+    Think step by step.""",
 )
 
 QA_PROMPT = """Here is the context:
 Your are given a list pairs of text documents and their sources as CONTEXT {data}.
 Find the most relevant document that matches the QUERY {query} and give a detailed `ANSWER` by including the `SOURCE` filename.
 You are allowed to show an relevant code from the context. In case you don't the answer say 'I don't know!'
-## EXAMPLE:
-CONTEXT: [('MAX graph api enables building computational graphs with ease', 'graph-api.md'), ('Install MAX with `modular install max`', 'install.md')]
-QUERY: "MAX graph api"
-ANSWER: 'MAX graph api enables building computational graphs with ease', SOURCE: graph-api.md
-## GO"""
+"""
 
 DATA_PATH = Path(os.path.dirname(os.path.dirname(__file__))) / "data"
 model_state = st.empty()
@@ -121,6 +119,7 @@ def start_llama3(
             "--min-p",
             str(min_p),
         ]
+        print(' '.join(command))
         if custom_ops_path:
             command.append("--custom-ops-path")
             command.append(custom_ops_path)
@@ -230,13 +229,69 @@ if prompt := st.chat_input("Ask Questions about the your Docs"):
                 "content": QA_PROMPT.format(**{"data": data, "query": prompt}),
             }
         ]
+        start_api_time = datetime.datetime.now()
+
         stream = client.chat.completions.create(
-            model="",
+            model="LLama3b",
             messages=messages,  # type: ignore
             stream=True,
+            stream_options={"include_usage": True}
         )
-        response = st.write_stream(stream)
 
-    st.session_state.messages.append(
-        {"role": "assistant", "content": response, "avatar": "🦙"}
+        streamed_response: str = ''
+        stream_container = st.empty()
+        first_text = True
+        isFirstChunk = True
+        ttft_msg = ''
+        for chunk in stream:
+            if(isFirstChunk):
+                end_api_time = datetime.datetime.now()
+                ttft_msg = f'''TTFT: {str("{:.2f}".format(((end_api_time - start_api_time).total_seconds())))}s
+
+'''
+                streamed_response+= ttft_msg
+                stream_container.markdown(ttft_msg)
+                isFirstChunk = False
+            if chunk.usage != None:
+                end_time = datetime.datetime.now()
+                total_time_taken = (end_time - start_api_time).total_seconds()
+
+                usage = f'''
+### Usage statistics: 
+
+{ttft_msg}
+
+prompt_tokens: {str(chunk.usage.prompt_tokens)}
+
+completion_tokens: {str(chunk.usage.completion_tokens)}
+
+total_tokens: {str(chunk.usage.total_tokens)}
+
+total time taken: {str(total_time_taken)}s
+
+T/s: {str("{:.2f}".format(chunk.usage.total_tokens / total_time_taken))}
+'''
+                streamed_response+= usage
+            if len(chunk.choices) == 0 or chunk.choices[0].delta is None:
+                continue;
+            else:
+                chunk = (chunk.choices[0].delta.content)
+                if not chunk:
+                    # Empty strings can be ignored
+                    continue
+                streamed_response += chunk
+                # Only add the streaming symbol on the second text chunk
+                stream_container.markdown(
+                    streamed_response + ("" if first_text else _TEXT_CURSOR),
+                )
+                first_text = False
+
+        stream_container = st.empty()
+
+        st.session_state.messages.append(
+        {"role": "assistant", "content": streamed_response, "avatar": "🦙"}
     )
+
+
+
+
